fc0b44f8a61c1e0ee6fcb1528d06162f692ef577
Mateo <daniel10saujil@gmail.com>
2023-11-12 14:25:22 -0300

    Agregada el API para usuarios

---
+++ WebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,51 @@
+﻿using Back.Dominio;
+using Back.Fachada.Implementacion;
+using Back.Fachada.Interfaz;
+using Back.Login;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        public IAplicacion app;
+
+        public UsuarioController()
+        {
+            app = new Aplicacion();
+        }
+
+        [HttpPost("/usuario")]
+        public IActionResult PostUsuario(Usuario oUsuario)
+        {
+            try
+            {
+                if (oUsuario == null)
+                    return BadRequest("Se debe ingresar un usuario");
+                return Ok(app.SaveUsuario(oUsuario));
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno. Intente más tarde");
+            }
+        }
+
+        [HttpPost("/verificar")]
+        public IActionResult GetAuth(Usuario oUsuario) 
+        {
+            try
+            {
+                if (oUsuario == null)
+                    return BadRequest("Se debe ingresar un usuario");
+                return Ok(app.CheckUsuario(oUsuario));
+            }
+            catch
+            {
+                return StatusCode(500, "Error interno. Intente más tarde");
+            }
+        }
+    }
+}
